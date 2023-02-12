59f3e398875956d980ee2063de33e8be07d4e39d
Andrew Cottam <andrew@restor.eco>
2023-02-12 12:21:59 +0100

    bug fix in paths in restore files

--- automation/service-management/start_services.py
+++ automation/service-management/start_services.py
@@ -2,8 +2,8 @@ import subprocess, requests, os
 
 # Start all the Cloud Run services and Cloud SQL
 path = os.path.dirname(os.path.realpath(__file__)) 
-result = subprocess.run(['sh', path + os.sep + 'start_services.sh'], stdout=subprocess.PIPE)
-result.stdout
+# result = subprocess.run(['sh', path + os.sep + 'start_services.sh'], stdout=subprocess.PIPE)
+# result.stdout
 
 # Request the Geoserver start page - this will block until Geoserver is ready
 print('Waiting for Geoserver to be up..')
@@ -11,5 +11,5 @@ requests.get('https://geoserver-ny43uciwwa-oc.a.run.app/geoserver/web/')
 print('Geoserver is up')
 # Restore the database
 print('Restoring the database')
-result = subprocess.run(['sh', '/Users/andrewcottam/Documents/GitHub/gcloud-library/automation/restore_1.sh'], stdout=subprocess.PIPE)
+result = subprocess.run(['sh', path + os.sep + 'restore_1.sh'], stdout=subprocess.PIPE)
 result.stdout
