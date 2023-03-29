697b84c91ace2f219919902a592cce814f996b4b
Andrew Cottam <a.cottam@gmail.com>
2023-03-29 13:41:29 +0200

    added a wait for quota expiry

--- automation/service-management/delete_retired_revisions.py
+++ automation/service-management/delete_retired_revisions.py
@@ -1,4 +1,5 @@
 # Deletes all retired revisions across all Cloud Run services - install run_v2 with pip install google-cloud-run
+import time
 from google.cloud import run_v2
 from google.cloud.run_v2.types import Condition
 
@@ -24,6 +25,10 @@ def delete_retired_revisions():
                     operation = client.delete_revision(request=request)
                     write_requests += 1
                     print(f"Deleted ({write_requests} write requests)")
+                    if (write_requests >60):
+                        # There is a quota of 60 write operations per minute using the Cloud Run API, so wait 1 minute before continuing
+                        time.sleep(60)
+                        print('Pausing to wait for the quota limit per minute to expire..')
                 else:
                     print('Revision is currently serving')
 
