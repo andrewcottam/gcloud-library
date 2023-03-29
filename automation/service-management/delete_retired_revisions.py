e69259e3fde5f672e38d863ce87e349e3e271943
Andrew Cottam <a.cottam@gmail.com>
2023-03-29 13:38:45 +0200

    minor edit

--- automation/service-management/delete_retired_revisions.py
+++ automation/service-management/delete_retired_revisions.py
@@ -10,6 +10,7 @@ def delete_retired_revisions():
     # request = run_v2.ListRevisionsRequest(parent="projects/162666128137/locations/europe-west8/services/tree-detection-server",)
     # Make the request to get the list of revisions
     page_result = client.list_revisions(request=request)
+    write_requests = 0
     # Iterate through the revisions and if they are retired, delete them
     for i, revision in enumerate(page_result):
         print(f"\nRevision: {revision.name[revision.name.rfind('/')+1:]}")
@@ -21,7 +22,8 @@ def delete_retired_revisions():
                     request = run_v2.DeleteRevisionRequest(name=revision.name,)
                     # Make the request to delete the revision
                     operation = client.delete_revision(request=request)
-                    print("Deleted")
+                    write_requests += 1
+                    print(f"Deleted ({write_requests} write requests)")
                 else:
                     print('Revision is currently serving')
 
