3c5cd5bb968508f27d521e311d5c20288e242370
andrewcottam <a.cottam@gmail.com>
2024-04-14 11:28:50 +0200

    handle quota limit

--- automation/service-management/delete_executions.py
+++ automation/service-management/delete_executions.py
@@ -13,9 +13,16 @@ def delete_executions():
     # Make the request
     page_result = client.list_executions(request=request)
     # Handle the response
+    write_requests = 0    
     for response in page_result:
         request = run_v2.DeleteExecutionRequest(name=response.name)
         operation = client.delete_execution(request=request)
+        write_requests += 1
+        if (write_requests >59):
+            # There is a quota of 60 write operations per minute using the Cloud Run API, so wait 1 minute before continuing
+            print('Pausing to wait for the quota limit per minute to expire..')
+            time.sleep(60)
+            write_requests = 0
         print(f"Deleting {response.name}...")
         response = operation.result()
 
